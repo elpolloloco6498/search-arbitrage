3fc5f3e09bb292aa73b23a7c8ebc251991dadb41
LE TENOUX--RACHIDI Isham <isham.le.tenoux@etu.imt-lille-douai.fr>
2022-11-09 20:24:17 +0100

    finding negative cycles in the graph

--- main.py
+++ main.py
@@ -14,12 +14,10 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
-
-exchange = ccxt.binance()
-
+import math
 
 def load_static_data_into_file(exchange):
-    markets = exchange.fetch_markets()[:100]
+    markets = exchange.fetch_markets()[:200]
     # select markets that are active and type=spot
     data = {"pair": [], "base": [], "quote": []}
     # fill the dataframe with market data
@@ -82,94 +80,137 @@ def symbol_to_matrix_id(df):
             j += 1
     return symbol_to_id
 
+
 def generate_adjacency_matrix(df, symbol_to_id):
     nb_nodes = len(symbol_to_id)
     adjacency_matrix = [[0] * nb_nodes for i in range(nb_nodes)]
 
     for i in range(len(df)):
         base, quote = df.loc[i, "base"], df.loc[i, "quote"]
+        ask, bid = df.loc[i, "ask"], df.loc[i, "bid"]
         id_base, id_quote = symbol_to_id[base], symbol_to_id[quote]
-        adjacency_matrix[id_base][id_quote] = 1  # the rate has to be changed using ticker information
-        adjacency_matrix[id_quote][id_base] = 1
-    return np.matrix(adjacency_matrix)
+        adjacency_matrix[id_base][id_quote] = ask  # the rate has to be changed using ticker information
+        adjacency_matrix[id_quote][id_base] = 1/bid
+    return np.matrix(adjacency_matrix, dtype=float)
 
-def plot_market_graph(adjacency_np_matrix, nodes_label_to_id):
-    graph = nx.from_numpy_matrix(adjacency_np_matrix)
 
-    # display graph
-    labelNodes = {v: k for k, v in nodes_label_to_id.items()}
-
-    nx.draw(graph, labels=labelNodes, with_labels=True, node_size=200, node_color='lightgray', edgecolors='red',
+def plot_market_graph(graph, nodes_label_to_id):
+    label_nodes = {v: k for k, v in nodes_label_to_id.items()}
+    nx.draw(graph, labels=label_nodes, with_labels=True, node_size=200, node_color='lightgray', edgecolors='red',
             font_size=10, width=0.5)
     plt.show()
 
 
-def find_negative_cycles():
+def negative_log(v):
+    if v != 0:
+        return -math.log(v)
+    else:
+        return 0
+
+
+def transform_matrix(matrix):
+    vec_func = np.vectorize(negative_log, otypes=[float])
+    return vec_func(matrix)
+
+
+def getW(edge):
+    return edge[2]['weight']
+
+
+def getNegativeCycle(g):
+    n = len(g.nodes())
+    edges = list(g.edges().data())
+    d = np.ones(n) * 10000
+    p = np.ones(n)*-1
+    x = -1
+    for i in range(n):
+        for e in edges:
+            if d[int(e[0])] + getW(e) < d[int(e[1])]:
+                d[int(e[1])] = d[int(e[0])] + getW(e)
+                p[int(e[1])] = int(e[0])
+                x = int(e[1])
+    if x == -1:
+        print("No negative cycle")
+        return None
+    for i in range(n):
+        x = p[int(x)]
+    cycle = []
+    v = x
+    while True:
+        cycle.append(str(int(v)))
+        if v == x and len(cycle) > 1:
+            break
+        v = p[int(v)]
+    return list(reversed(cycle))
+
+def find_negative_cycles(graph):
+    #label_nodes = {v: k for k, v in symbol_to_id.items()}
+    #directed_graph = graph.to_directed()
+    print(getNegativeCycle(graph))
+    """
+    simple_cycles = list(nx.simple_cycles(directed_graph))
+    for cycle in simple_cycles:
+        if len(cycle) > 2:
+            for node in cycle:
+                print(label_nodes[node], end=" ")
+            print("\n")"""
+
+
+def search_surface_rate_arbitrage():
     pass
 
 
-def find_arbitrage():
+def search_deep_orderbook_validated_arbitrage():
     pass
 
-load_static_data_into_file(exchange)
-df = read_data("./market_data.csv")
-get_price_data(exchange, df)
-symbol_to_id = symbol_to_matrix_id(df)
-matrix = generate_adjacency_matrix(df, symbol_to_id)
-plot_market_graph(matrix, symbol_to_id)
+
+def main():
+    # TODO calculate run time and print it
+    """
+    np.set_printoptions(precision=8, suppress=True)
+    exch = ccxt.binance()
+    #load_static_data_into_file(exch)  # load static data from the API
+    df = read_data("./market_data.csv")  # read the stored data
+    symbol_to_id = symbol_to_matrix_id(df)  # establish a correspondance relationship between the symbol and matrix ID
+    get_price_data(exch, df)  # gather price information from the API
+    matrix = generate_adjacency_matrix(df, symbol_to_id)  # generate the adjacency matrix of the problem
+    """
+    #potato:0 carrot:1 lettuce:2
+    matrix_food_market = np.matrix([
+        [0, 0.5, 0.5],
+        [2, 0, 0.5],
+        [2, 2, 0]
+    ])
+    #pound:0, dollar:1, yen:2
+    matrix_forex_market = np.matrix([
+        [0, 1/0.8, 100],
+        [0.8, 0, 1/0.013],
+        [1/100, 0.013, 0]
+    ])
+    print(matrix_forex_market)
+    transformed_matrix = transform_matrix(matrix_forex_market)
+    print(transformed_matrix)
+    print(transformed_matrix.shape)
+    sum = transformed_matrix[1,0]+transformed_matrix[0,2]+transformed_matrix[2,1]
+    print(sum)
+
+    graph = nx.from_numpy_matrix(matrix_forex_market)  # creating the graph representation via the adjacency matrix
+    #print(getNegativeCycle(graph))
+    print("negative cycle : ", nx.negative_edge_cycle(graph))
+    cycle = nx.find_negative_cycle(graph, 0)
+    print("negative cycle 2: ", cycle)
+    #plot_market_graph(graph, symbol_to_id)
+
+    # draw graph test
+    nx.draw(graph, node_size=200, node_color='lightgray', edgecolors='red',
+            font_size=10, width=0.5)
+    plt.show()
+
+if __name__ == "__main__":
+    main()
 
 
 """
-exchange = ccxt.binance()
-markets = exchange.fetch_markets()[:200]
-tickers = exchange.fetch_tickers()
-# pprint(tickers)
-#print(tickers['ETH/BTC'])
-
-# filter markets
-markets = [market for market in markets if market['type'] == 'spot' and market['active']]
-print(len(markets))
-
-# construct adjacency matrix for the markets
-# get label nodes
-nodes_label_to_id = dict()
-base_quote_relation = dict()
-
-i = 0
-for market in markets:
-    # select only spot markets that are active
-    # print(market)
-    base, quote = market['base'], market['quote']
-    base_quote_relation[base] = quote
-    if base not in nodes_label_to_id:
-        nodes_label_to_id[base] = i
-        i += 1
-    if quote not in nodes_label_to_id:
-        nodes_label_to_id[quote] = i
-        i += 1
-
-nb_nodes = len(nodes_label_to_id)
-adjacency_matrix = [[0] * nb_nodes for i in range(nb_nodes)]
-
-for market in markets:
-    base, quote = market['base'], market['quote']
-    id_base = nodes_label_to_id[base]
-    id_quote = nodes_label_to_id[quote]
-    adjacency_matrix[id_base][id_quote] = 1  # the rate has to be changed using ticker information
-    adjacency_matrix[id_quote][id_base] = 1
-
-adjacency_np_matrix = np.matrix(adjacency_matrix)
-# creating the network graph
-graph = nx.from_numpy_matrix(adjacency_np_matrix)
-
-# display graph
-labelNodes = {v: k for k, v in nodes_label_to_id.items()}
-
-nx.draw(graph, labels=labelNodes, with_labels=True, node_size=200, node_color='lightgray', edgecolors='red',
-        font_size=10, width=0.5)
-plt.show()
-
-
 # search cycles in the graph
 #cycle = nx.find_negative_cycle(graph, 0)
 
